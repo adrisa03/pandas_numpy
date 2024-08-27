b0b11041338f08c4f6e955bcb76401713e66bbff
Adriana Torres <aditorn@ymail.com>
2024-08-27 16:45:48 -0500

    se completo reto eL array

---
+++ elarray.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+#Crear Lista
+
+lista = [1,2,3,4,5]
+print (lista)
+np.array(lista)
+arr = np.array(lista)
+#print(arr)
+#print(type(arr))
+
+#Crear Matriz
+
+matriz = [[1,2,3] [4,5,6] [7,8,9]]
+matriz = np.array(matriz)
+#print(matriz)
+
+#print (arr[1])
+#print (arr[0] + arr[5])
+#print (matriz)
+#print (matriz[0])
+
+#print (matriz[1])
+#print (matriz[2])
+#print (matriz[3])
+
+#print(matriz [0,2])
+
+#print (arr[:6])
+#print (arr[2:])
+#print (arr[:])
+#print (arr[::3])
+#print (arr[-1])
+#print (arr[-3:])
+
+#print (matriz[1:])
+#print (matriz[1:,0:2])
+
+
